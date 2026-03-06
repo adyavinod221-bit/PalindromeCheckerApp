cc2977680640bb1d862fedba406fa496afb5a341
adyavinod221 <adyavinod221@gmail.com>
2026-03-06 10:04:24 +0530

    UC2 DONE

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,16 @@
+// File: UseCase2PalindromeCheckerApp.java
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println( "welcome to palindrome checker app");
-        System.out.println("version : 1.0");
-        System.out.println("system initialized successfully");
+        String word = "madam";
 
+        // Reverse the string
+        String reversed = new StringBuilder(word).reverse().toString();
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
+
