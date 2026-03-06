125f21a65dd1b165dce646c0e8051571f6b5f908
adyavinod221 <adyavinod221@gmail.com>
2026-03-06 10:15:00 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,27 @@
 // File: UseCase2PalindromeCheckerApp.java
 
 public class PalindromeCheckerApp {
+
+
     public static void main(String[] args) {
+
+        // Original string literal
         String word = "madam";
 
-        // Reverse the string
-        String reversed = new StringBuilder(word).reverse().toString();
-        if (word.equals(reversed)) {
+        // Reverse string using loop
+        String reversedWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            // String concatenation (+) builds a new String each time
+            reversedWord += word.charAt(i);
+        }
+
+        // Compare original and reversed using equals()
+        if (word.equals(reversedWord)) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
         }
     }
 }
+
 
